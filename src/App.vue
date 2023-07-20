f4f8e3fdd271bfda0e55a89d968d1a226b625a5d
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-07-20 06:14:30 +0100

    feat: create sidebar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<!-- <script setup>
-</script> -->
+<script setup>
+import Sidebar from './components/Sidebar.vue';
+</script>
 
-<template>
-  <div class="text-[30px]">igkhb,johnsoncodehk</div>
-</template>
+
+  <template>
+    
+    <Sidebar />
+    <div class="p-4 sm:ml-[13.5rem]">
+      <div>you are amaxinf Global Registration
+We can make components available globally in the current Vue application using the app.component() method:</div>
+<div>jhbujknm,uhjn</div>
+</div>
+
+
+    
+  </template>
 

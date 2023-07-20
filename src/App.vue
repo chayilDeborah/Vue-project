775f5c9c56a7d683961bc54758017f4dea78eba7
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-07-20 11:48:46 +0100

    feat: create navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,12 @@
 <script setup>
-import Sidebar from './components/Sidebar.vue';
+import Navbar from './components/Navbar.vue';
+import Sidebar from './components/Sidebar.vue'
 </script>
 
+<template>
+  <Sidebar />
+  <div class="sm:ml-[13.5rem]">
+  <Navbar />
 
-  <template>
-    
-    <Sidebar />
-    <div class="p-4 sm:ml-[13.5rem]">
-      <div>you are amaxinf Global Registration
-We can make components available globally in the current Vue application using the app.component() method:</div>
-<div>jhbujknm,uhjn</div>
-</div>
-
-
-    
-  </template>
-
+  </div>
+</template>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="flex justify-center bg-[#F0F4FA]">
+    <div class="w-full mt-[30px]">
+      <div class="flex justify-between ml-[20px]">
+        <div class="text-[32px] text-[#464A50] font-bold">Welcome Jonathan</div>
+        <div>
+          <button
+            id="dropdownDefaultButton"
+            data-dropdown-toggle="dropdown"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type="button"
+          >
+            Dropdown button
+            <svg
+              class="w-2.5 h-2.5 ml-2.5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 10 6"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="m1 1 4 4 4-4"
+              />
+            </svg>
+          </button>
+          <!-- Dropdown menu -->
+          <div
+            id="dropdown"
+            class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
+          >
+            <ul
+              class="py-2 text-sm text-gray-700 dark:text-gray-200"
+              aria-labelledby="dropdownDefaultButton"
+            >
+              <li>
+                <a
+                  href="#"
+                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >Dashboard</a
+                >
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >Settings</a
+                >
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >Earnings</a
+                >
+              </li>
+              <li>
+                <a
+                  href="#"
+                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                  >Sign out</a
+                >
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="mt-[15px] text-[#464A50] text-[17px] ml-[20px]">View assessments and top scorers at a glance</div>
+    </div>
+  </div>
+</template>

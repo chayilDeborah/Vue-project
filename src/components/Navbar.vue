4e888258b89ce3b3036056dae7e270a032b34df8
chayilDeborah <aniyikayeolamide3@gmail.com>
2023-07-21 12:08:24 +0100

    fix: nav dropdown

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,72 +1,44 @@
+<script>
+export default {
+  data() {
+    return {
+      isDropdownOpen: false,
+      selectedOption: null,
+      options: ["New assessment", "SimpliedUX workbook"]
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.isDropdownOpen = !this.isDropdownOpen;
+    },
+    selectOption(option) {
+      this.selectedOption = option;
+      this.isDropdownOpen = false;
+    },
+  },
+};
+</script>
+
+
 <template>
   <div class="flex justify-center bg-[#F0F4FA]">
     <div class="w-full mt-[30px]">
       <div class="flex justify-between ml-[20px]">
         <div class="text-[32px] text-[#464A50] font-bold">Welcome Jonathan</div>
-        <div>
-          <button
-            id="dropdownDefaultButton"
-            data-dropdown-toggle="dropdown"
-            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-            type="button"
-          >
-            Dropdown button
-            <svg
-              class="w-2.5 h-2.5 ml-2.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 10 6"
-            >
-              <path
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="m1 1 4 4 4-4"
-              />
-            </svg>
-          </button>
-          <!-- Dropdown menu -->
-          <div
-            id="dropdown"
-            class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700"
-          >
-            <ul
-              class="py-2 text-sm text-gray-700 dark:text-gray-200"
-              aria-labelledby="dropdownDefaultButton"
-            >
-              <li>
-                <a
-                  href="#"
-                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >Dashboard</a
-                >
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >Settings</a
-                >
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >Earnings</a
-                >
-              </li>
-              <li>
-                <a
-                  href="#"
-                  class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                  >Sign out</a
-                >
-              </li>
-            </ul>
-          </div>
-        </div>
+        <div class="relative">
+    <!-- Display selected option -->
+    <button @click="toggleDropdown" class="block py-2 px-4 rounded-md bg-blue-500 text-white">
+      {{ selectedOption || 'Create Assessments' }}
+    </button>
+
+    <!-- Dropdown content -->
+    <div v-if="isDropdownOpen" class="absolute top-0 left-0 mt-[40px] py-2 px-4 bg-white border border-gray-300 rounded-md">
+      <!-- Loop through options -->
+      <div v-for="option in options" :key="option" @click="selectOption(option)" class="text-[14px] cursor-pointer mt-[5px] hover:bg-gray-100">
+        {{ option }}
+      </div>
+    </div>
+  </div>
       </div>
       <div class="mt-[15px] text-[#464A50] text-[17px] ml-[20px]">View assessments and top scorers at a glance</div>
     </div>
